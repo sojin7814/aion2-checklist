de7e20d7c234e28ab0bfefc46e2741698e8f093a
sojin7814 <mozzii0780@gmail.com>
2026-03-23 17:20:50 +0900

    Add files via upload

---
+++ caddy/sw.js
@@ -0,0 +1,32 @@
+var CACHE_NAME = 'caddy-income-v1';
+var urlsToCache = [
+  '/caddy/',
+  '/caddy/guide.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) { return name !== CACHE_NAME; })
+          .map(function(name) { return caches.delete(name); })
+      );
+    })
+  );
+});
